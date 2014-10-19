d01ace149156a0d7d018af6772d650f5fdee8509
Matt Dougherty <mattdougherty@Matts-MacBook-Air.local>
2014-10-18 22:10:59 -0400

    need new logo and links to gh

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$('.to-home').click(function() {
+    $('body').animate({
+        scrollTop: $('.header-wrapper').offset().top
+    }, 800);
+});
+
+$('.to-about').click(function() {
+    $('body').animate({
+        scrollTop: $('.mission-wrapper').offset().top
+    }, 800);
+});
+
+$('.to-contact').click(function() {
+    $('body').animate({
+        scrollTop: $('.contact-wrapper').offset().top
+    }, 800);
+});
+
+$('.to-invest').click(function() {
+    $('body').animate({
+        scrollTop: $('.get-involved').offset().top
+    }, 800);
+});
+
+$('.to-tool').click(function() {
+    $('body').animate({
+        scrollTop: $('.contact-wrapper').offset().top
+    }, 800);
+});
